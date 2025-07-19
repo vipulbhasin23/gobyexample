0df28d40f37c8ff6fc37ad2df6cfc128c0ca0f98
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-19 10:20:37 +0530

    Arrays completion

--- arrays.go
+++ arrays.go
@@ -12,4 +12,24 @@ func main() {
 	fmt.Println("get:", a[4])
 
 	fmt.Println("Length: ", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("b: ", b)
+
+	b = [...]int{5, 6, 7, 8, 9}
+	fmt.Println("b: ", b)
+
+	b = [...]int{100, 3: 400, 500}
+	fmt.Println("b: ", b)
+
+	var twoD [2][3]int
+	for i := range 2 {
+		for j := range 3 {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("2D: ", twoD)
+
+	twoD = [2][3]int{{1, 2, 3}, {1, 2, 3}}
+	fmt.Println("2D: ", twoD)
 }

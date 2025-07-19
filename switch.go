a921e0ad655bb623866820a436f21a7f8691cf48
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-19 09:57:12 +0530

    Switch without expression

--- switch.go
+++ switch.go
@@ -24,4 +24,12 @@ func main() {
 	default:
 		fmt.Println("It's a weekday")
 	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
 }

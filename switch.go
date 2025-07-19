78251dbc6d3e63b53e9c9ccca6cbf65db55ef137
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-19 09:52:38 +0530

    Basic switch

---
+++ switch.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 2
+	fmt.Println("Write ", i, " as: ")
+
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+}

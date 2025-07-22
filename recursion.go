826d98042ffc3c1998a3650a5fc3cf0a8e53c9d2
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-22 17:23:13 +0530

    recursion contd.

--- recursion.go
+++ recursion.go
@@ -11,4 +11,14 @@ func fact(n int) int {
 
 func main() {
 	fmt.Println(fact(7))
+
+	var fib func(n int) int
+
+	fib = func(n int) int {
+		if n < 2 {
+			return n
+		}
+		return fib(n-1) + fib(n-2)
+	}
+	fmt.Println(fib(7))
 }

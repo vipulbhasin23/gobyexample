7e1c0cb3e0c2eace7e2376e0e07e5a41b0aab938
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-24 13:13:58 +0530

    Struct pointers

--- structs.go
+++ structs.go
@@ -21,4 +21,13 @@ func main() {
 
 	fmt.Println(&person{name: "Ann", age: 40})
 	fmt.Println(newPerson("Jon"))
+
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
 }

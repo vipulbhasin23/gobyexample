689d07f4e7fe20c3300bf75c8412a2a7bd764765
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-24 12:50:24 +0530

    Count and print runes in string

--- strings-runes.go
+++ strings-runes.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	const s = "สวัสดี"
 
 	fmt.Println("Length:", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+	fmt.Println()
+
+	fmt.Println("Rune count in string:", utf8.RuneCountInString(s))
+
+	for idx, runeVal := range s {
+		fmt.Printf("%#U starts at index: %d\n", runeVal, idx)
+	}
 }

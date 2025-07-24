a34fc51952703c252a0c1b1dd89550360086ee05
Vipul Bhasin <vipulbhasin22@gmail.com>
2025-07-24 13:10:56 +0530

    Structs - contd

--- structs.go
+++ structs.go
@@ -15,4 +15,10 @@ func newPerson(name string) *person {
 
 func main() {
 	fmt.Println(person{"Bob", 20})
+
+	fmt.Println(person{name: "Alice", age: 30})
+	fmt.Println(person{name: "Fred"})
+
+	fmt.Println(&person{name: "Ann", age: 40})
+	fmt.Println(newPerson("Jon"))
 }
